6152454e867836cab505aceb7d90b47bda7a510f
Onkar58 <onkarwaghmode58@gmail.com>
2023-09-14 11:48:32 +0530

    Firebase Auth and Mongo Data

---
+++ client/src/components/Signup/Signup.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import { auth } from '../../firebase/auth'
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import axios from 'axios'
+
+const sendData = async (data) => {
+
+
+  const userDetails = {
+    username: data.username,
+    name: data.name,
+    email: data.email,
+    password: data.password,
+    photoURL: data.photoURL || '',
+    // userId: createUser.user.uid
+  }
+  axios.post('http://localhost:8000/api/v1/auth/signup', userDetails)
+  .then((res) => { console.log("Success", res) })
+  .catch((err) => { console.log("Error", err) })
+}
+
+const Signup = () => {
+  const [form, setForm] = useState({})
+  const provider = new GoogleAuthProvider()
+  const saveData = async (e) => {
+    e.preventDefault()
+    const createUser = await createUserWithEmailAndPassword(auth, form.email, form.password)
+    console.log(createUser, typeof createUser);
+    createUser.user.displayName = form.username
+    console.log(createUser.user.displayName, "User name here");
+    if (createUser) {
+      sendData(form)
+      
+    }
+    console.log(createUser);
+    console.log('saveData')
+  }
+
+  const handledata = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+    // console.log(form)
+  }
+
+  const handleOAuth = async () => {
+    const user = await signInWithPopup(auth, provider)
+
+    const data = {
+      username: user.user.displayName,
+      name: user.user.displayName,
+      email: user.user.email,
+      password: user.user.uid,
+      photoURL: user.user.photoURL,
+      // userId: createUser.user.uid
+    }
+    sendData(data)
+    console.log(user.user.photoURL, "User name here");
+  }
+
+  return (
+    <div>
+      <form onSubmit={saveData}>
+        <input type='email' name="email" placeholder='Email' onChange={handledata} />
+        <input type='text' name="username" placeholder='Username' onChange={handledata} />
+        <input type='text' name="name" placeholder='Name' onChange={handledata} />
+        <input type='password' name="password" placeholder='Password' onChange={handledata} />
+        <button type='submit'>Signup</button>
+      </form>
+      <button onClick={handleOAuth}>Google Signin</button>
+    </div>
+  )
+}
+
+export default Signup
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 
 const {login,signup} = require("../controllers/user");
+const userModel = require("../models/user.model");
 
 
 // ********************************************************************************************************
@@ -10,7 +11,24 @@ const {login,signup} = require("../controllers/user");
 // ********************************************************************************************************
 
 // Route for user login
-router.post("/login", login)
+router.post("/signup", async (req, res) => {
+
+    console.log(req.body);
+    const user = new userModel(req.body);
+    const userExists = await userModel.findOne({email: req.body.email}).exec();
+    console.log("User Exists", userExists);
+    if (!userExists){
+        user.save()
+        res.json(req.body);
+    }
+    else{
+        return res.json({
+            success: false,
+            error: "User already exists",
+            message: "User already exists",
+        })
+    }
+})
 
 // Route for user signup
 router.post("/signup", signup)
