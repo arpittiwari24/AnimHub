1f55a54b53f6e1c0333a9bdab5c5e6bdde822034
Spyware007 <omgawandeofficial9834899149@gmail.com>
2023-09-17 17:52:07 +0530

    client structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,33 @@
-import './App.css'
-import Signup from './components/Signup/Signup'
-import Login from './components/Login/Login'
-import { Routes, Route, useNavigate } from 'react-router-dom'
-import { useEffect } from 'react'
-import { auth } from './firebase/auth'
-import Homepage from './components/Homepage/Homepage'
-// import {useNavigate} from 'react-router-dom'
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { auth } from "./firebase/auth";
+import Homepage from "./components/Homepage";
 
 function App() {
-  const navigator = useNavigate()
-  // if (!auth.currentUser) {
-  //   console.log("User not logged in ");
-  //   // return
-  // }
-  // if (auth.currentUser) {
-  //   console.log("User logged in ");
-  //   // return
-  // }
+  const navigator = useNavigate();
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
-      if (user){
+      if (user) {
         console.log("User logged in ");
-        navigator("/")  
-      }
-      else{
+        navigator("/");
+      } else {
         console.log("User not logged in ");
-        navigator("/login")
+        navigator("/login");
       }
-    })
-  }, [])
+    });
+  }, []);
   return (
     <>
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      {/* <Route path="/signup" element={<Signup />} /> */}
-      <Route path="/" element={<Homepage />} />
-    </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        {/* <Route path="/signup" element={<Signup />} /> */}
+        <Route path="/" element={<Homepage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
