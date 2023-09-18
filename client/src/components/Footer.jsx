52df4e035143c5bfb1976f13dce8261f2f2464c9
Sahil-Rathod <sahilrathod2810@gmail.com>
2023-09-19 00:04:31 +0530

    testing branch

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div class="h-[50vh] w-full bg-[#151515]">
+        <div class="footer-main">
+          <div class="first">
+            <h3 class="font-3xl">Information</h3>
+            <p>Sitemap</p>
+            <p>Terms & Condition</p>
+            <p>Privacy Policy</p>
+            <p>DMCA</p>
+            <p>2257</p>
+            <p>ECU</p>
+          </div>
+        </div>
+        <div class="second"></div>
+        <div class="footer-copyright"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
