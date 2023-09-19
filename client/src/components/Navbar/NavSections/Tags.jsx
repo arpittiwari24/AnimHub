c10e6bc9d72dda59fb2e5f589aa756bae6e8e126
Spyware007 <omgawandeofficial9834899149@gmail.com>
2023-09-20 00:51:21 +0530

    tags and promostrip added

--- client/src/components/Navbar/NavSections/Tags.jsx
+++ client/src/components/Navbar/NavSections/Tags.jsx
@@ -3,15 +3,117 @@ import React from "react";
 const Tags = () => {
   return (
     <>
-      <div className="w-full h-full flex justify-between items-center">
-        <div className="w-[25%] h-full flex flex-col justify-center items-center bg-[red]">
-          Hello
+      <div className="w-full h-full flex flex-col justify-between items-center py-6 px-10">
+        <div className="w-full h-full flex flex-col justify-start items-start">
+          <h1 className="text-2xl font-bold">Most Trending Tags</h1>
+          <ul className="flex flex-wrap justify-start items-center gap-4 mt-4">
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Buttons
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Inputs
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Loaders
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Checkboxes
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Cards
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Toggles
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Forms
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              More coming soon...
+            </li>
+          </ul>
         </div>
-        <div className="w-[60%] h-full flex flex-col justify-center items-center bg-[green]">
-          Hello
+        <div className="w-full h-full flex flex-col justify-start items-start">
+          <h1 className="text-2xl font-bold">Common Tags</h1>
+          <ul className="flex flex-wrap justify-start items-center gap-4 mt-4">
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Buttons
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Inputs
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Loaders
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Checkboxes
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Cards
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Toggles
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Forms
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              More coming soon...
+            </li>
+          </ul>
         </div>
-        <div className="w-[15%] h-full flex flex-col justify-center items-center bg-[blue]">
-          Hello
+        <div className="w-full h-full flex flex-col justify-start items-start">
+          <h1 className="text-2xl font-bold">Other Tags</h1>
+          <ul className="flex flex-wrap justify-start items-center gap-4 mt-4">
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Buttons
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Inputs
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Loaders
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Checkboxes
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Cards
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Toggles
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Forms
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              More coming soon...
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Buttons
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Inputs
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Loaders
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Checkboxes
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Cards
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Toggles
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              Forms
+            </li>
+            <li className="bg-[#151515] hover:bg-[#2b2b2b] font-bold  px-5 py-3 relative rounded-lg">
+              More coming soon...
+            </li>
+          </ul>
         </div>
       </div>
     </>
---
+++ client/src/components/Navbar/PromoStrip.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PromoStrip = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center px-24 py-4">
+        <h3 className="flex justify-center items-center border-[0.05px] gap-2 border-[#5d5d5d] text-lg font-semibold text-[#fff] px-52 py-2 rounded-sm">
+          AnimHub is particpaiting in{" "}
+          <span className="text-primary font-bold">HacktoberFest 2023</span>
+        </h3>
+      </div>
+    </>
+  );
+};
+
+export default PromoStrip;
