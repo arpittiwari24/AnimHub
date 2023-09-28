74b5f6d4acafd965e58fc4d125003984b07ac01c
Onkar58 <onkarwaghmode58@gmail.com>
2023-09-28 16:22:14 +0530

    Editor Page tracking

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CodeEditor from "../components/common/Editor/CodeEditor";
 import CodeOutput from "../components/common/CodeOutput";
+import ReactGA from "react-ga4";
 
 const Editor = () => {
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname })
+  }, [])
   return (
   <div>
     <CodeEditor />
