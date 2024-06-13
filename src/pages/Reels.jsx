c03476be15cfd7a597251d1ca94a006cf6e8e98f
arpittiwari24 <atofficial2410@gmail.com>
2024-06-13 17:51:14 +0530

    modified reels

--- src/pages/Reels.jsx
+++ src/pages/Reels.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getVerifiedComponents } from "../apis/components.api";
 import { usePremiumContext } from "../context/IsPremiumContextProvider";
 import { useUserContext } from "../context/UserContextProvider";
@@ -9,8 +9,7 @@ import ReactGA from "react-ga4";
 import "./Reels.css"
 
 export default function Reels() {
-  // const data = [
-  // ];
+
   const [componentsData, setComponentsData] = useState([]);
   const [loading, setLoading] = useState(true);
   const {premium} = usePremiumContext()
@@ -33,20 +32,32 @@ export default function Reels() {
     handleData();
   }, []);
 
+  const vidRef = useRef()
+
+  useEffect(() => {
+    const scroll = document.getElementById("video-container");
+
+    if (scroll) {
+      scroll.addEventListener("scroll", () => {
+        vidRef.current.pause();
+      });
+    }
+  }, []);
+
 
   return (
-    <div className="grid place-items-center h-screen">
+    <div className="App">
       <center>
           <div
             className="video-container w-full"
           >
-            <div className="reels py-10">
+            <div className="reels py-10 bg-black">
             {componentsData
               .filter((component) => !component.premium || premium)
               .map((card, index) => (
-                <div key={""} className="">
-                  <ComponentCard key={index} data={card} />
-                </div>
+                  <div key={index} className="py-20 flex gap-10 items-center justify-center">
+                    <ComponentCard ref={vidRef} key={Math.floor(Math.random(index) * componentsData.length)} data={card} loop/>
+                  </div>
               ))}
             {premium &&
               userData?.email !== undefined &&
